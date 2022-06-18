c77d95f525c8211284e3a0cb83dbe6bca6f78ac3
2096779623 <admin@utermux.dev>
2022-06-18 19:57:24 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,34 +1,11 @@
-const express = require('express');
-const timeout = require('connect-timeout');
-const { createProxyMiddleware } = require('http-proxy-middleware');
-const app = express();
+const express = require('express')
+const { createProxyMiddleware }  = require('http-proxy-middleware')
 
+const app = express()
 
-const HOST = 'https://mirrors.utermux.dev', PORT = '80';
+app.all('/*', createProxyMiddleware({
+  target: process.env.TARGET_URL,
+  pathRewrite: { '^/': '', }
+}))
 
-
-const TIME_OUT = 30 * 1e3;
-
-
-app.set('port', PORT);
-
-
-app.use(timeout(TIME_OUT));
-app.use((req, res, next) => {
-  if (!req.timedout) next();
-});
-
-
-app.use('/', express.static('static'));
-
-
-app.use(createProxyMiddleware('/', {
-  target: HOST, 
-  changeOrigin: true, 
-  ws: false
-}));
-
-
-app.listen(app.get('port'), () => {
-  console.log(`server running ${PORT }`);
-});
+app.listen(process.env.PORT)
