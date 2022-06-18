7dcd340e38810d67ce72b8337f140ab55bd36421
2096779623 <admin@utermux.dev>
2022-06-18 19:44:26 +0800

    fc

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const timeout = require('connect-timeout');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+const app = express();
+
+
+const HOST = 'https://mirrors.utermux.dev', PORT = '443';
+
+
+const TIME_OUT = 30 * 1e3;
+
+
+app.set('port', PORT);
+
+
+app.use(timeout(TIME_OUT));
+app.use((req, res, next) => {
+  if (!req.timedout) next();
+});
+
+
+app.use('/', express.static('static'));
+
+
+app.use(createProxyMiddleware('/', {
+  target: HOST, 
+  changeOrigin: true, 
+  ws: false
+}));
+
+
+app.listen(app.get('port'), () => {
+  console.log(`server running ${PORT }`);
+});
